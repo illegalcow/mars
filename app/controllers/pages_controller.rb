d3ce4fe5e602f70b2c104ed01c16b192f006dba5
Stian Grefstad <stian@grefstad.com>
2017-07-26 10:11:00 +0000

    your commet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
-  def home
-  end
+    def home
+    end
+
+    def random
+     random_texts = ["nikita", "hustling'", "kake," "Eh", "yogurt", "police", "pannekake"]
+    
+        @random_text = random_texts.sample
+    end
 end
